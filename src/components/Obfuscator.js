f5e6a1c982387dbf1ab1ef42f7d1dc4673c0fa8d
Alina Pereskokova <Alina.Pereskokova@emc.com>
2019-09-20 14:07:54 +0300

    create basic input and interface

--- src/components/Obfuscator.js
+++ src/components/Obfuscator.js
@@ -1,25 +1,41 @@
 import React, {Component} from 'react';
+import cloneDeep from 'lodash';
 import {Button, Form} from "react-bootstrap";
 
 class Obfuscator extends Component {
 
   constructor(props) {
     super(props);
-
+    this.state = {
+      inputText: '',
+      outputText: 'skdfhkjfsdkl'
+    }
   }
 
-  obfuscateText = () => {
+  obfuscateText = (e) => {
+    this.setState({outputText: this.state.inputText});
+  };
 
+  handleChange = (e) => {
+    this.setState({inputText: e.target.value})
   };
 
   render() {
-    return <Form>
+    let inputText = '';
+    return (
+      <Form>
       <Form.Group controlId="exampleForm.ControlInput1">
         <Form.Label>Text for obfuscate</Form.Label>
-        <Form.Control placeholder="empty..." />
+        <Form.Control placeholder="empty..." value={this.state.inputText} onChange={this.handleChange}/>
+        <Button onClick={this.obfuscateText}>Obfuscate</Button>
       </Form.Group>
-      <Button onClick={this.obfuscateText}>Obfuscate</Button>
-    </Form>;
+
+      <Form.Group controlId="exampleForm.ControlOutput">
+        <Form.Label>Obfuscated text</Form.Label>
+        <Form.Control placeholder="empty..." value={this.state.outputText}/>
+      </Form.Group>
+    </Form>
+    )
   }
 }
 
