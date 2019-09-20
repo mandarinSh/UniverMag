11b0d480f7ff83a2b29449724e07aed3cda30f65
Alina Pereskokova <Alina.Pereskokova@emc.com>
2019-09-20 17:04:45 +0300

    refactoring

--- src/common/Common.js
+++ src/common/Common.js
@@ -1,29 +1,18 @@
 
 String.prototype.obfs = function(key, n = 126) {
-  // return String itself if the given parameters are invalid
-  if (!(typeof(key) === 'number' && key % 1 === 0)
-    || !(typeof(key) === 'number' && key % 1 === 0)) {
-    return this.toString();
-  }
+  let charArray = this.toString().split('');
 
-  let chars = this.toString().split('');
-
-  for (let i = 0; i < chars.length; i++) {
-    let c = chars[i].charCodeAt(0);
+  for (let i = 0; i < charArray.length; i++) {
+    let c = charArray[i].charCodeAt(0);
 
     if (c <= n) {
-      chars[i] = String.fromCharCode((chars[i].charCodeAt(0) + key) % n);
+      charArray[i] = String.fromCharCode((charArray[i].charCodeAt(0) + key) % n);
     }
   }
 
-  return chars.join('');
+  return charArray.join('');
 };
 
 String.prototype.deobfs = function(key, n = 126) {
-  if (!(typeof(key) === 'number' && key % 1 === 0)
-    || !(typeof(key) === 'number' && key % 1 === 0)) {
-    return this.toString();
-  }
-
   return this.toString().obfs(n - key);
 };
