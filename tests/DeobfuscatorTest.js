14e9589f961b9e4b71e91de52a1c630ff8fc3993
Alina Pereskokova <Alina.Pereskokova@emc.com>
2019-12-09 20:42:19 +0300

    add tests

---
+++ tests/DeobfuscatorTest.js
@@ -0,0 +1,29 @@
+import { Selector } from 'testcafe'; // first import testcafe selectors
+import { ReactSelector } from 'testcafe-react-selectors';
+
+fixture `Getting Started`// declare the fixture
+  .page `http://localhost:3000`;  // specify the start page
+
+
+//then create a test and place your code there
+test('My first test', async t => {
+
+  const inputText = ReactSelector('input').withProps(
+    {automationid: 'text-for-deobf'}
+  );
+
+  const obfBtn = ReactSelector('button').withProps(
+    {automationid: 'deobf-btn'}
+  );
+
+  const obfText = ReactSelector('input').withProps(
+    {automationid: 'deobf-text'}
+  );
+
+  await t
+    .typeText(inputText, 'LZNLNLMN')
+    .click(obfBtn)
+
+    // Use the assertion to check if the actual header text is equal to the expected one
+    .expect(obfText.value).eql('fthfhfgh');
+});
